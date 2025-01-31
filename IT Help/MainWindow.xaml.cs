2a41e41ce5c9e180fb187bad99ed52d441b2a582
Patrik Šoma <soma.patrik@gmail.com>
2025-01-31 16:17:34 +0100

    styles

--- IT Help/MainWindow.xaml.cs
+++ IT Help/MainWindow.xaml.cs
@@ -1,20 +1,8 @@
 ﻿using IT_Help.Models;
-using System.Text;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace IT_Help
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
